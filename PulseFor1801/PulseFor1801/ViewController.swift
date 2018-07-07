b4b7ec4fd5e000a22b49e78e8f6232337250e066
manjunadhb <manjunadhb@gmail.com>
2018-07-07 18:25:29 +0530

    committing initial files.

---
+++ PulseFor1801/PulseFor1801/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PulseFor1801
+//
+//  Created by Manjunadh Bommisetty on 07/07/18.
+//  Copyright © 2018 Manjunadh Bommisetty. All rights reserved.
+// This is my comment.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
